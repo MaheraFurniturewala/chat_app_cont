8aa9a370e7cd0e32e4f15ea700b06ae6dabe4202
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-06-07 17:41:33 +0530

    continue from passport

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore=require('connect-mongo')(session);
+const sassMiddleware=require('node-sass-middleware');
+app.use(sassMiddleware({
+    // source=where do i pick up the scss files to convert to css
+    src: '/assets/scss',
+    //where do i need to put my css files
+    dest: '/assets/css',
+    // whatever info you see while you see in the terminal when server is running do i want to display errors in compilation 
+    // you would put it to false when in production mode
+    debug: true,
+    //do i want everything to be in a single line or do i want it to be in multiple lines
+    outputStyle:'extended',
+    //where should my server look for css files
+    prefix: '/css'
+}));
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use('/', require('./routes'));
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
